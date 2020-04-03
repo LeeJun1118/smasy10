e52595d2cb6b7c1adb3127ed502309459e6159ea
ljun1118 <ejun1118@gmail.com>
2020-04-03 14:18:15 +0900

    프로젝트 init

---
+++ src/test/java/com/smasy10/apple/AppleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smasy10.apple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
