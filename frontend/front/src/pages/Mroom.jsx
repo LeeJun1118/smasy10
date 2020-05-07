c3aacf6dadaf54b04374973a805bfd7398700a8a
pc <bomine>
2020-05-07 16:50:51 +0900

    방만들기 폼

--- frontend/front/src/pages/Mroom.jsx
+++ frontend/front/src/pages/Mroom.jsx
@@ -1,4 +1,8 @@
 import React, {useEffect, useState} from "react";
+import {Form, Button} from "react-bootstrap";
+import Calendar from "react-calendar";
+import 'react-calendar/dist/Calendar.css';
+import '../css/Mroom.css';
 import customAxios from "../customAxios";
 
 const Mroom = () => {
@@ -12,11 +16,67 @@ const Mroom = () => {
             customAxios('/makeAroom', callback);
         }, []
     );
+
+    function simulateNetworkRequest() {
+        return new Promise((resolve) => setTimeout(resolve, 2000));
+    }
+
+    //캘린더 관련 함수
+    const state = {
+        date: new Date(),
+    }
+    const onDateChange = date => {
+        this.setState({date});
+        console.log(date);
+    }
+    ////
+
     return(
         <div className="Mroom">
-            방 만드는 페이지
-            <br/>
-            이 기기의 IP주소는 {ip}입니다.
+            <Form>
+                <Form.Group controlId="exampleForm.ControlSelect1" id="sports">
+                    <Form.Label>종목</Form.Label>
+                    <Form.Control as="select">
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group controlId="exampleForm.ControlSelect1" id="area">
+                    <Form.Label>지역</Form.Label>
+                    <Form.Control as="select">
+                        <option>a</option>
+                        <option>b</option>
+                        <option>c</option>
+                        <option>d</option>
+                        <option>e</option>
+                    </Form.Control>
+                </Form.Group>
+
+                <Form.Group id="date">
+                    <Form.Label>날짜</Form.Label>
+                        <Calendar />
+                </Form.Group>
+
+                <Form.Group controlId="exampleForm.ControlSelect1" id="facility">
+                    <Form.Label>시설</Form.Label>
+                    <Form.Control as="select">
+                        <option>A</option>
+                        <option>B</option>
+                        <option>C</option>
+                        <option>D</option>
+                        <option>E</option>
+                    </Form.Control>
+                </Form.Group>
+                {/* 각 시설 위치 지도 보여주면 좋을 듯*/}
+
+                <Button variant="dark" type="submit">개설하기</Button>
+            </Form>
+
+            <br/>이 기기의 IP주소는 {ip}입니다.
         </div>
     )
 }
