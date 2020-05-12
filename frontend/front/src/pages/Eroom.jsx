65260110eac5ee17fbb141c12126d859d18d1f20
pc <bomine>
2020-05-12 16:22:03 +0900

    EroomComponent

--- frontend/front/src/pages/Eroom.jsx
+++ frontend/front/src/pages/Eroom.jsx
@@ -8,23 +8,23 @@ const Eroom = () => {
 
             <Table striped bordered hover id="table">
                 <thead>
-                <tr><th>#</th><th>종목</th><th>지역</th><th>날짜</th><th>시설</th><th>인원</th></tr>
+                <tr><th>#</th><th>종목</th><th>지역</th><th>시설</th><th>날짜</th><th>인원</th></tr>
                 </thead>
                 <tbody>
                 <tr>
                     <td>1</td>
                     <td>축구</td>
                     <td>부산 남구</td>
-                    <td>2020-06-12</td>
                     <td>경성대학교 건학기념관 운동장</td>
+                    <td>2020-06-12</td>
                     <td>1</td>
                 </tr>
                 <tr>
                     <td>2</td>
                     <td>야구</td>
                     <td>부산 부산진구</td>
-                    <td>2020-05-19</td>
                     <td>공원 야구장</td>
+                    <td>2020-05-19</td>
                     <td>3</td>
                 </tr>
                 </tbody>
