3df051ea451d2367dfbe31d1fe41c14f2d510cc8
ljun1118 <ejun1118@gmail.com>
2020-05-28 21:58:50 +0900

    방 개설된다...

--- src/main/java/com/smasy10/apple/config/SecurityConfig.java
+++ src/main/java/com/smasy10/apple/config/SecurityConfig.java
@@ -110,7 +110,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/**/*.css",
                         "/**/*.js")
                 .permitAll()
-                .antMatchers("/auth/**", "/oauth2/**")
+                .antMatchers("/auth/**", "/oauth2/**","/api/**")
                 .permitAll()
                 .anyRequest()
                 .authenticated()
--- src/main/java/com/smasy10/apple/security/TokenProvider.java
+++ src/main/java/com/smasy10/apple/security/TokenProvider.java
@@ -10,7 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-
 import java.util.Date;
 
 @Service
