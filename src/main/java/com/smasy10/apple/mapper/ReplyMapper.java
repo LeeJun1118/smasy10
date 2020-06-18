5762dcd9118a843545315f8b315c48bf8bb5581a
ljun1118 <ejun1118@gmail.com>
2020-06-18 12:39:31 +0900

    ing : 리뷰 목록 하는 중

--- src/main/java/com/smasy10/apple/mapper/ReplyMapper.java
+++ src/main/java/com/smasy10/apple/mapper/ReplyMapper.java
@@ -1,12 +1,15 @@
 package com.smasy10.apple.mapper;
 
 import com.smasy10.apple.domain.Reply;
+import com.smasy10.apple.domain.dto.PlaceReviewDto;
 import com.smasy10.apple.domain.dto.ReplyDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.ManyToOne;
 
 @Configuration
 @Mapper(
@@ -17,4 +20,7 @@ import org.springframework.context.annotation.Configuration;
 public interface ReplyMapper {
     @Mapping(target = "user")
     ReplyDto toReplyDto(Reply reply);
+
+    @Mapping(target = "place")
+    PlaceReviewDto toPlaceReviewDto(Reply reply);
 }
--- src/main/java/com/smasy10/apple/repository/ReplyRepository.java
+++ src/main/java/com/smasy10/apple/repository/ReplyRepository.java
@@ -1,13 +1,25 @@
 package com.smasy10.apple.repository;
 
 import com.smasy10.apple.domain.*;
+import com.smasy10.apple.domain.dto.PlaceReviewDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
+import static com.smasy10.apple.controller.PlaceController.FIND_ALL_PLACE_SEARCH_REVIEW;
+
 public interface ReplyRepository extends JpaRepository<Reply,Long> {
+    /*public final static String FIND_BY_ID_STATE = "SELECT  FROM Table1 a RIGHT JOIN a.table2Obj b " +
+            "WHERE b.column = :id" +
+            "AND a.id NOT IN (SELECT c.columnFromA from a.table3Obj c where state = :state)";
+
+    @Query(FIND_BY_ID_STATE)
+    public List<Alert> findXXXXXXXX(@Param("id") Long id, @Param("state") Long state);
+*/
+
 
 /*
     @Query("FROM Reply r WHERE r.room =: roomId ")
@@ -21,4 +33,14 @@ public interface ReplyRepository extends JpaRepository<Reply,Long> {
     void deleteAllByRoom(Room room);
 
     List<Reply> findAllByPlace(Place place);
+
+    public final static Long zero = 0L;
+
+    @Query("FROM Reply r WHERE r.place >= :place ")
+    List<Reply> findAllByPlaceId(@Param("place")Place place);
+
+    //List<Reply> findAllByOrderByDate();
+
+   /* @Query(FIND_ALL_PLACE_SEARCH_REVIEW)
+    List<Reply> findAllByText(@Param("text") String text);*/
 }
