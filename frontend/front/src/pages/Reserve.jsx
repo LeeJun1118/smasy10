cc6b5a2cdfa75311f8193a98d90a8d68a1bc40c1
pc <bomine>
2020-06-18 14:50:55 +0900

    .

--- frontend/front/src/pages/Reserve.jsx
+++ frontend/front/src/pages/Reserve.jsx
@@ -22,6 +22,7 @@ class Reserve extends Component {
                     reservations: data
                 })
             }).catch(error => {
+
                 Alert.error((error && error.message) || 'Oops! Something went wrong. Please try again!');
             });
     }
